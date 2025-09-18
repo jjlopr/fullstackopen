b9fb385a55f0058ab1ee88027b7e60ed8c21c322
jjlopr <julian@aside.design>
2025-09-18 20:27:43 +0200

    Console message

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,4 +1,5 @@
-const App = () => {
+ const App = () => {
+  console.log('Hello from component')
   return (
     <div>
       <p>Hello world</p>
@@ -7,3 +8,5 @@ const App = () => {
 }
 
 export default App
+
+// I promise to keep the console open all the time during this course, and for the rest of my life when I'm doing web development.
