3b2e7a9d2122f7d113415fca40f01e69f025023c
jjlopr <julian@aside.design>
2025-09-18 22:31:17 +0200

    Component using Fragments

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -20,13 +20,19 @@ const Footer = () => {
   )
 }
 
-// Component with an array of components
+// Component using Fragments
 const App = () => {
-  return [
-    <h1>Greetings</h1>,
-    <Hello name='Maya' age={26 + 10} />,
-    <Footer />
-  ]
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  )
 }
 
 export default App
