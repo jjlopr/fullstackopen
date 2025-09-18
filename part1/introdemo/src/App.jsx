8b061d4da07a2a07503f9c072af20fdcc25aa65f
jjlopr <julian@aside.design>
2025-09-18 20:35:36 +0200

    Never delete export default App

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,12 +1,17 @@
- const App = () => {
-  console.log('Hello from component')
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
   )
 }
 
 export default App
-
-// I promise to keep the console open all the time during this course, and for the rest of my life when I'm doing web development.
