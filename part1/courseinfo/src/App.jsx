73b54cabbbe1d08371800ea32ef6dc551a42203d
jjlopr <julian@aside.design>
2025-09-22 14:10:15 +0200

    Exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,29 +1,31 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  // Exercise 1.4 Sending the entire parts Array as a single prop
-  // Instead of breaking it down into individual properties
+  // Exercise 1.5 Change the course and its parts into a single JavaScript object
+  // Fix everything that breaks
   return (
     <div>
       <Header course={course} />
-
-      <Content parts={parts} />
       
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -33,16 +35,16 @@ const Header = (props) => {
   
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   )
 }
 
 const Content = (props) => {
   console.log(props)
-  
+  // No need to pass course.parts as props calls "parts" = course.parts scope
   return (
-    <div>
+    <div>  
       <Part namex={props.parts[0].name} exercisex={props.parts[0].exercises} />
       <Part namex={props.parts[1].name} exercisex={props.parts[1].exercises} />
       <Part namex={props.parts[2].name} exercisex={props.parts[2].exercises} />
