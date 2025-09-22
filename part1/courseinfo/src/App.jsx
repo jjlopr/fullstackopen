5c69605a407eadb966fb2859b6917faf6383f939
jjlopr <julian@aside.design>
2025-09-22 13:13:02 +0200

    Refactor App, Header and Content

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,43 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course}/>
 
       <Content 
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1} 
-        exercises2={exercises2} 
-        exercises3={exercises3} 
+        part1={part1.name}
+        part2={part2.name}
+        part3={part3.name}
+        exercises1={part1.exercises} 
+        exercises2={part2.exercises} 
+        exercises3={part3.exercises} 
       />
       
-      <Total 
+      {/* <Total 
         exercises1={exercises1} 
         exercises2={exercises2} 
         exercises3={exercises3} 
-      />
+      /> */}
     </div>
   )
 }
 
 const Header = (props) => {  
+  console.log(props)
+  
   return (
     <>
       <h1>{props.course}</h1>
@@ -39,31 +47,34 @@ const Header = (props) => {
 
 // Exercise 1.2 – Independent Part & Content components 
 const Content = (props) => {
+  console.log(props)
+  
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
+      <Part namex={props.part1} exercisex={props.exercises1} />
+      <Part namex={props.part2} exercisex={props.exercises2} />
+      <Part namex={props.part3} exercisex={props.exercises3} />
     </div>
   )
 }
 
 // Using Destructuring
-const Part = ({ name, exercises }) => {
+const Part = ({ namex, exercisex }) => {
   return (
-    <p>{name}, <i>{exercises} exercises</i></p>
+    <p>{namex}, <i>{exercisex} exercises</i></p>
   )
 }
 
-const Total = (props) => {
-  const total = props.exercises1 + props.exercises2 + props.exercises3
-
-  return (
-    <footer>
-      <i>Total exercises: {total}</i>
-    </footer>
-  )
-}
+// const Total = (props) => {
+//   const total = props.exercises1 + props.exercises2 + props.exercises3
+//   console.log(props)
+  
+//   return (
+//     <footer>
+//       <i>Total exercises: {total}</i>
+//     </footer>
+//   )
+// }
 
 
 export default App
