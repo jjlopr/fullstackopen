7b7499c6049e0a4a2a77bdd5c90eb8a25e6ad144
jjlopr <julian@aside.design>
2025-09-24 16:29:41 +0200

    Part 1.c Event Handler - clicked

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
-  setTimeout(    () => setCounter(counter + 1),    1000  )
-
-  // You can debug the app by logging the values of the component's variables to the console
-  console.log('mostrando...', counter)
 
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+
+      <button onClick={() => console.log('clicked')}>
+        plus
+      </button>
+    </div>
   )
 }
 
