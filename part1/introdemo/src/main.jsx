8d91a870a69ac1b6d51e409de1f01a3adc4811c9
jjlopr <julian@aside.design>
2025-09-24 16:05:14 +0200

    Part 1.c useState render stateful component

--- part1/introdemo/src/main.jsx
+++ part1/introdemo/src/main.jsx
@@ -2,17 +2,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const refresh = () => {
-  root.render(
-    <App counter={counter} />
-  )
-}
-// setInterval https://developer.mozilla.org/en-US/docs/Web/API/Window/setInterval
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
