d57b5d3062b3f7bfc94ef3186f64388ef2a586d9
jjlopr <julian@aside.design>
2025-09-24 16:25:17 +0200

    Part 1.c useState render + console.logfor debug

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
   setTimeout(    () => setCounter(counter + 1),    1000  )
+
+  // You can debug the app by logging the values of the component's variables to the console
+  console.log('mostrando...', counter)
+
   return (
     <div>{counter}</div>
   )
