90923ddfc25669ef86b71c96f34df518709d2dcb
jjlopr <julian@aside.design>
2025-09-24 17:16:13 +0200

    Part 1.c Display component created using props

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,15 +1,22 @@
+// Part 1.c – Refactor to small reusable components
 import { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)    
-  
   const setToZero = () => setCounter(0)
+
+  // Component display using props
+  const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
 
   return (
     <div>
-      <div>{counter}</div>
+      <Display counter={counter}/>
 
       <button onClick={increaseByOne}>
         plus
