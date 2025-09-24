248a8953affca6a48c9ade52b278e9a105c2c562
jjlopr <julian@aside.design>
2025-09-24 16:39:35 +0200

    Part 1.c Event Handler counter onClick and Reset button

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -3,13 +3,15 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
+  // Increase counter and Reset counter button
   return (
     <div>
       <div>{counter}</div>
 
-      <button onClick={() => console.log('clicked')}>
-        plus
-      </button>
+    <button onClick={() => setCounter(counter + 1)}>
+      plus
+    </button>
+    <button onClick={() => setCounter(0)}>         zero      </button>    
     </div>
   )
 }
