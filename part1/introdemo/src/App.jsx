cee890ff95d7fc6d8f20859c6dbdb08a6b00ad92
jjlopr <julian@aside.design>
2025-09-27 18:13:36 +0200

    Part 1.d – Code simplifyed and Reset button added

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,4 +1,4 @@
-// Part 1.d – Simplify the functions avoiding variable assignation
+// Part 1.d – Code simplifyed and Reset button added
 import { useState } from 'react'
 
 const App = () => {
@@ -6,34 +6,22 @@ const App = () => {
 		left: 0, right: 0
 	})
 	
-  // Assigning the object to a variable in the event handlers is not necessary
-	// const handleLeftClick = () => {
-	// 	const newClicks = {
-	// 		...clicks,
-	// 		left: clicks + 1
-	// 	}
-	// 	setClicks(newClicks)
-	// }
 	const handleLeftClick = () =>
   setClicks({ ...clicks, left: clicks.left + 1 })
 
-  // Assigning the object to a variable in the event handlers is not necessary
-	// const handleRightClick = () => {
-	// 	const newClicks = {
-	// 		...clicks,
-	// 		right: clicks.right + 1
-	// 	}
-	// 	setClicks(newClicks)
-	// }
   const handleRightClick = () =>
   setClicks({ ...clicks, right: clicks.right + 1 })
+
+  const handleReset = () =>
+  setClicks({left: 0, right: 0})
 	
 	return (
 		<div>
 			{clicks.left}
 			<button onClick={handleLeftClick}>left</button>
 			<button onClick={handleRightClick}>right</button>
-	{clicks.right}
+	    {clicks.right}
+      <button onClick={handleReset} disabled={clicks.left === 0 && clicks.right === 0}>reset</button>
 		</div>
 	)
 }
