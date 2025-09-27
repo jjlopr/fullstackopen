ef7db9ff2263a470857c92e837a1778ce1ad098e
jjlopr <julian@aside.design>
2025-09-27 15:27:11 +0200

    Part 1.d – Object spread syntax (...)

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,34 +1,35 @@
-// Part 1.d – Saving the click count of left & right buttons into a single object
+// Part 1.d – Object spread syntax (...)
 import { useState } from 'react'
+
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
-
-  const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
-  }
-
-  const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
-  }
-
-  return (
-    <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
-    </div>
-  )
+	const [clicks, setClicks] = useState({
+		left: 0, right: 0
+	})
+	
+	const handleLeftClick = () => {
+		const newClicks = {
+			...clicks,
+			left: clicks + 1
+		}
+		setClicks(newClicks)
+	}
+	
+	const handleRightClick = () => {
+		const newClicks = {
+			...clicks,
+			right: clicks.right + 1
+		}
+		setClicks(newClicks)
+	}
+	
+	return (
+		<div>
+			{clicks.left}
+			<button onClick={handleLeftClick}>left</button>
+			<button onClick={handleRightClick}>right</button>
+	{clicks.right}
+		</div>
+	)
 }
 
 
