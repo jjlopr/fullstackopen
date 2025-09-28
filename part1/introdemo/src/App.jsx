d01bc303389432c4883f13e54436a17be0c2eb83
jjlopr <julian@aside.design>
2025-09-28 14:58:23 +0200

    Part 1.d – Handling arrays with concat. Reset button added

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,27 +1,48 @@
-// Part 1.d – Code simplifyed and Reset button added
+// Part 1.d – Handling arrays and Reset button
 import { useState } from 'react'
 
 const App = () => {
-	const [clicks, setClicks] = useState({
-		left: 0, right: 0
-	})
+	// const [clicks, setClicks] = useState({
+	// 	left: 0, right: 0
+	// })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  // Every click is stored in a separate piece of state called allClicks that is initialized as an empty array []
+  const [allClicks, setAll] = useState([])
 	
-	const handleLeftClick = () =>
-  setClicks({ ...clicks, left: clicks.left + 1 })
+	// const handleLeftClick = () =>
+  // setClicks({ ...clicks, left: clicks.left + 1 })
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)  
+  }
 
-  const handleRightClick = () =>
-  setClicks({ ...clicks, right: clicks.right + 1 })
+  // const handleRightClick = () =>
+  // setClicks({ ...clicks, right: clicks.right + 1 })
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
-  const handleReset = () =>
-  setClicks({left: 0, right: 0})
+  // const handleReset = () =>
+  // setClicks({left: 0, right: 0})
+  const handleReset = () => {
+    setAll([])      // Reset to empty array
+    setRight(0)
+    setLeft(0)
+  }
 	
 	return (
 		<div>
-			{clicks.left}
+			{left}
 			<button onClick={handleLeftClick}>left</button>
 			<button onClick={handleRightClick}>right</button>
-	    {clicks.right}
-      <button onClick={handleReset} disabled={clicks.left === 0 && clicks.right === 0}>reset</button>
+	    {right}
+      {// <button onClick={handleReset} disabled={clicks.left === 0 && clicks.right === 0}>reset</button>
+      }
+      <button onClick={handleReset} disabled={left === 0 && right === 0}>reset</button>
+      <p>{allClicks.join('-')}</p>
 		</div>
 	)
 }
